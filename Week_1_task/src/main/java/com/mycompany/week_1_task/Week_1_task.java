52182152e021307f1452874a027109fba3435ed8
shmuradd <109430694+shmuradd@users.noreply.github.com>
2023-12-18 19:53:53 +0400

    Add files via upload

---
+++ Week_1_task/src/main/java/com/mycompany/week_1_task/Week_1_task.java
@@ -0,0 +1,71 @@
+package com.mycompany.week_1_task;
+
+public class Week_1_task {
+
+    public static void main(String[] args) {
+        //Reverse String
+//        String str1="Salam";
+//        for(int i=str1.length()-1;i>=0;i--)
+//        {
+//            System.out.print(str1.charAt(i));
+//        }
+
+        //word with the most letter
+//        String[] str_array={"Murad","Ali","Jamal","Mahammad","Yusif"};
+//        int index=0;
+//        int len=str_array[0].length();
+//        for (int i = 1; i < str_array.length-1; i++) {
+//            if (str_array[i].length()>len) {
+//                len=str_array[i].length();
+//                index=i;
+//            }
+//        }
+//        System.out.println(str_array[index]);
+//        System.out.println(len);
+
+        //Sum of even numbers between (x,y)
+//        int x = 10; //these are boundaries I will not consider them in my code
+//        int y = 98;
+//        int sum = 0;
+//        for (int i = x+1; i < y; i++) {
+//            if (i % 2 == 0) {
+//                sum += i;
+//            }
+//        }
+//        System.out.println("Sum of even numbers between x and y is");
+//        System.out.println(sum);
+
+        //power 
+//        int a=2;
+//        int b=3;
+//        int power=1;
+//        for (int i = 0; i < b; i++) {
+//            power*=a;
+//        }
+//        System.out.println("a^b=");
+//        System.out.println(power);
+
+
+        //there is a factor of 2. Find which factor is this
+//        int a=1024;
+//        int b=0;
+//        
+//        while(a>1)
+//        {
+//            a/=2;
+//            b++;
+//            
+//        }
+//        System.out.println(b);
+
+        //Find the average of int array
+//        int[] arr={1,2,34,3,2,3,2,42,4};
+//        float sum=0;
+//        int len=arr.length;
+//        for (int i = 0; i < len; i++) {
+//         sum+=arr[i];   
+//        }
+//        System.out.println("Average is");
+//        System.out.println(sum/len);
+//    }
+}
